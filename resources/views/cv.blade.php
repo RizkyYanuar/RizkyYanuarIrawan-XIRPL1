4d3e76cb0ffedba6a0b041943e0c41651b9dc67f
Viserys89 <fistvicky@gmail.com>
2023-08-24 13:52:46 +0700

    menambahkan skill dan memperbaiki tentang saya

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -27,7 +27,7 @@
                 <div class="col">
                     <h5 class="fw-bold">Keahlian</h5>
                 <div class="list-keahlian">
-                    <p>HTML <br> CSS <br> JS <br> Mobile Legenda <br> PABAJI MOBAIL</p>
+                    <p>HTML <br> CSS <br> JS <br> PHP <br> Mobile Legenda <br> PABAJI MOBAIL </p>
                 </div>
                 </div>
             </div>
@@ -44,7 +44,7 @@
                     <div class="row">
                         <div class="col">
                             <h3 class="fw-bold">Tentang Saya</h3>
-                        <p>Saya adalah seorang murid di SMK 2 Bandung. Usia saya saat saya membuat cv ini adalah 16 tahun. Saat ini saya tengah berada di kelas 11 pada jurusan Rekayasa Perangkat Lunak. </p>
+                        <p>Saya adalah seorang murid di SMK 2 Bandung. Usia saya saat saya membuat cv ini adalah 16 tahun. Saat ini saya tengah berada di kelas 11 pada jurusan Rekayasa Perangkat Lunak. Saya tertarik dengan pemrograman berbasis website, seperti html, css, js, php, laravel, dan react. saat ini saya sedang dalam proses belajar untuk memahami bahasa pemrograman yang sulit melalui internet. </p>
                         </div>
                     </div>
                     <div class="row">
@@ -66,7 +66,48 @@
                     <div class="row">
                         <div class="col">
                             <h3 class="fw-bold">Pengalaman</h3>
-                            <p>Tidak ada</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <ul>
+                                <li>Membuat website dinamis sebagai tugas akhir pada kelas 10 <a href="#">Lihat Disini</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <h3 class="fw-bold">Skill</h3>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="skills-container">
+                            <div class="skill-bar">
+                                <div class="skill-label">HTML</div>
+                                <div class="skill-progress" data-skill="85" style="--skill: 70;"></div>
+                            </div>
+                            <div class="skill-bar">
+                                <div class="skill-label">CSS</div>
+                                <div class="skill-progress" data-skill="85" style="--skill: 70;"></div>
+                            </div>
+                            <div class="skill-bar">
+                                <div class="skill-label">JavaScript</div>
+                                <div class="skill-progress" data-skill="50" style="--skill: 50;"></div>
+                            </div>
+                            <div class="skill-bar">
+                                <div class="skill-label">PHP</div>
+                                <div class="skill-progress" data-skill="50" style="--skill: 50;"></div>
+                            </div>
+                            <div class="skill-bar">
+                                <div class="skill-label">Mobile Legend</div>
+                                <div class="skill-progress" data-skill="80" style="--skill: 80;"></div>
+                            </div>
+                            <div class="skill-bar">
+                                <div class="skill-label">PABAJI</div>
+                                <div class="skill-progress" data-skill="70" style="--skill: 70;"></div>
+                            </div>
+                    </div>
                         </div>
                     </div>
                 </div>
