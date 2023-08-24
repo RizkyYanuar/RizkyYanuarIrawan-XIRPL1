c337dfc21c3751b6eb99e5775cf255a464298048
Viserys89 <fistvicky@gmail.com>
2023-08-24 13:23:33 +0700

    first commit

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV | Rizky Yanuar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/cv.css') }}">
+</head>
+<body>
+    <div class="sidebar bg-dark text-center text-white">
+        <div class="container-fluid p-0">
+            <div class="row img-wrapper justify-content-center">
+                <div class="img-sidebar"><img src="{{ asset('img/kyy.jpeg') }}" alt="" class="rounded-circle img-sidebar">
+                </div>
+            </div>
+            <div class="row">
+                <h3 class="fw-bold">Rizky Yanuar Irawan</h3>
+            </div>
+            <div class="row">
+                <h5>Murid</h5>
+            </div>
+            <div class="row">
+                <p>Murid di Salah satu sekolah yang ada di bandung, yaitu SMKN 2 Bandung. Jurusan Rekayasa Perangkat Lunak</p>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h5 class="fw-bold">Keahlian</h5>
+                <div class="list-keahlian">
+                    <p>HTML <br> CSS <br> JS <br> Mobile Legenda <br> PABAJI MOBAIL</p>
+                </div>
+                </div>
+            </div>
+            <div class="row">
+                <h5 class="fw-bold">Kontak</h5>
+                <p>rizkyyanuarirawan@gmail.com <br> +62 821 2636 7502</p>
+            </div>
+        </div>
+    </div>
+    <div class="main-content text-dark">
+        <div class="hero">
+            <div class="container p-4">
+                <div class="hero-text">
+                    <div class="row">
+                        <div class="col">
+                            <h3 class="fw-bold">Tentang Saya</h3>
+                        <p>Saya adalah seorang murid di SMK 2 Bandung. Usia saya saat saya membuat cv ini adalah 16 tahun. Saat ini saya tengah berada di kelas 11 pada jurusan Rekayasa Perangkat Lunak. </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <h3 class="fw-bold">Pendidikan</h3>
+                            <ul>
+                                <li>
+                                    <p>SDN 194 Sukajadi | 2013 - 2019</p>
+                                </li>
+                                <li>
+                                    <p>SMPN 6 Bandung | 2019 - 2022</p>
+                                </li>
+                                <li>
+                                    <p>SMKN 2 Bandung | 2022 - 2025</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <h3 class="fw-bold">Pengalaman</h3>
+                            <p>Tidak ada</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
